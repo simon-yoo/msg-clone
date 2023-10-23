fa41b1ce82e0ed437245f68fccbd9a330ae6db6e
Simon Yoo <yoosghn0@gmail.com>
2023-10-23 15:23:03 +1300

    added AuthSocialButton

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -4,7 +4,8 @@ import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import Input from '@/app/components/input/Input';
 import Button from '@/app/components/Button';
-
+import AuthSocialButton from './AuthSocialButton';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
@@ -81,6 +82,28 @@ const AuthForm = () => {
             <div className='absolute inset-0 flex items-center'>
               <div className='w-full border-t border-gray-300' />
             </div>
+            <div className='relative flex justify-center text-sm'>
+              <span className='bg-white px-2 text-gray-500'>
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className='mt-6 flex gap-2'>
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction('github')}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction('google')}
+            />
+          </div>
+        </div>
+        <div className='flex gap-2 justify-center text-sm mt-5 px-2 text-gray-500'>
+          <div>
+            {variant === 'LOGIN'
+              ? 'New to Messenger?'
+              : 'Already have an account'}
           </div>
         </div>
       </div>
