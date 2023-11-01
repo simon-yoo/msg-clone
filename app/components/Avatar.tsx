e2d67f139846ead81716a14e786568d07360e2ab
Simon Yoo <yoosghn0@gmail.com>
2023-11-02 12:51:51 +1300

    config images avatar

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+'user client'
 
-const Avatar = () => {
-  return <div>Avatar</div>;
-};
+import { User } from '@prisma/client'
+import Image from 'next/image'
 
-export default Avatar;
+interface AvatarProps {
+  user?: User
+}
+
+const Avatar: React.FC<AvatarProps> = ({ user }) => {
+  return (
+    <div className='relative'>
+      <div className='relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11'>
+        <Image
+          src={user?.image || '/images/placeholder.jpg'}
+          alt='Avatar'
+          fill
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Avatar
--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,21 +1,21 @@
-'use client';
+'use client'
 
-import useRoutes from '@/app/hooks/useRoutes';
-import { useState } from 'react';
-import DesktopItem from './DesktopItem';
+import useRoutes from '@/app/hooks/useRoutes'
+import { useState } from 'react'
+import DesktopItem from './DesktopItem'
 
-import { User } from '@prisma/client';
-import Avatar from '../Avatar';
+import { User } from '@prisma/client'
+import Avatar from '../Avatar'
 
 interface DesktopSidebarProps {
-  currentUser: User;
+  currentUser: User
 }
 
 const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
-  const routes = useRoutes();
-  const [isOpen, setIsOpen] = useState(false);
+  const routes = useRoutes()
+  const [isOpen, setIsOpen] = useState(false)
 
-  console.log({ currentUser });
+  console.log({ currentUser })
   return (
     <div className='hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between'>
       <nav className='mt-4 flex flex-col justify-between'>
@@ -41,7 +41,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
         </div>
       </nav>
     </div>
-  );
-};
+  )
+}
 
-export default DesktopSidebar;
+export default DesktopSidebar
