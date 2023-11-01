c1802498857d305f9bc34c32ddce3a0adcee40e3
Simon Yoo <yoosghn0@gmail.com>
2023-11-02 12:21:05 +1300

    sidebar error

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ const Sidebar = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
   return (
     <div className='h-full'>
-      <DesktopSidebar currentUser={currentUser} />
+      <DesktopSidebar currentUser={currentUser!} />
       <MobileFooter />
       <main className='lg:pl-20 h-full'>{children}</main>
     </div>
