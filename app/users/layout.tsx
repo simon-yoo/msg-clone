173237a7ca02be9e9a74d258bca4bdd1ec09b5ee
Simon Yoo <yoosghn0@gmail.com>
2023-11-02 13:15:36 +1300

    userlist

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,10 +1,12 @@
 import getUsers from '../actions/getUsers'
 import Sidebar from '../components/sidebar/Sidebar'
+import UserList from './components/UserList'
 
 const UsersLayout = async ({ children }: { children: React.ReactNode }) => {
   const users = await getUsers()
 
   return (
+    // @ts-expect-erro server component
     <Sidebar>
       <div className='h-full'>
         <UserList items={users} />
