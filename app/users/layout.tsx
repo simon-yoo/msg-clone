cff6b143129f53c92dce2bee28a36a8f8f2427c3
Simon Yoo <yoosghn0@gmail.com>
2023-11-02 13:05:53 +1300

    getusers.ts

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,11 +1,18 @@
-import Sidebar from '../components/sidebar/Sidebar';
+import getUsers from '../actions/getUsers'
+import Sidebar from '../components/sidebar/Sidebar'
 
 const UsersLayout = async ({ children }: { children: React.ReactNode }) => {
+  const users = await getUsers()
+
   return (
     <Sidebar>
-      <div className='h-full'>{children}</div>;
+      <div className='h-full'>
+        <UserList items={users} />
+        {children}
+      </div>
+      ;
     </Sidebar>
-  );
-};
+  )
+}
 
-export default UsersLayout;
+export default UsersLayout
