40f97e38f379a0bb80f6871e78bfa71c99dcd430
Simon Yoo <yoosghn0@gmail.com>
2023-11-07 13:42:17 +1300

    updated conversation api

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -36,6 +36,28 @@ export async function POST(request: Request) {
         sender: true,
       },
     })
+
+    const updatedConversation = await prisma.conversation.update({
+      where: {
+        id: conversationId,
+      },
+      data: {
+        lastMessageAt: new Date(),
+        messages: {
+          connect: {
+            id: newMessage.id,
+          },
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            seen: true,
+          },
+        },
+      },
+    })
   } catch (error: any) {
     console.log(error, 'ERROR_MESSAGES')
     return new NextResponse('InternalError', { status: 500 })
