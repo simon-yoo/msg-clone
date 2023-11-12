1437dc42e6a1da69e1fd72f982f20d55e231ed1c
Simon Yoo <yoosghn0@gmail.com>
2023-11-12 17:13:17 +1300

    css

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -23,32 +23,34 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
         >
           <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
         </Transition.Child>
-        <div className='flex min-h-full items-center justify-center p-4 text-center sm:p-0'>
-          <Transition.Child
-            as={Fragment}
-            enter='ease-out duration-300'
-            enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-            enterTo='opacity-100 translate-y-0 sm:scale-100'
-            leave='ease-in duration-200'
-            leaveFrom='opacity-100  translate-y-0 sm:scale-100'
-            leaveTo='opcity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-          >
-            <Dialog.Panel className='relative tranform overflow-hidden rounded-lg bg-white px-4 pb-4 text-left shadow-xl transition-all w-full sm:my-8 sm:w-full sm:max-w-lg sm:p6'>
-              <div className='absolute right-0 top-0 hidden pr-4 pt-4 sm:block z-10'>
-                <button
-                  type='button'
-                  className='rounded-md bg-white hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2'
-                  onClick={onClose}
-                >
-                  <span className='sr-only'>
-                    <IoClose className='h-6 w-6' />
-                    Close
-                  </span>
-                </button>
-              </div>
-              {children}
-            </Dialog.Panel>
-          </Transition.Child>
+        <div className='fixed inset-0 z-10 overflow-y-auto'>
+          <div className='flex min-h-full items-center justify-center p-4 text-center sm:p-0'>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+              enterTo='opacity-100 translate-y-0 sm:scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100  translate-y-0 sm:scale-100'
+              leaveTo='opcity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+            >
+              <Dialog.Panel className='relative tranform overflow-hidden rounded-lg bg-white px-4 pb-4 text-left shadow-xl transition-all w-full sm:my-8 sm:w-full sm:max-w-lg sm:p6'>
+                <div className='absolute right-0 top-0 hidden pr-4 pt-4 sm:block z-10'>
+                  <button
+                    type='button'
+                    className='rounded-md bg-white hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2'
+                    onClick={onClose}
+                  >
+                    <span className='sr-only'>
+                      <IoClose className='h-6 w-6' />
+                      Close
+                    </span>
+                  </button>
+                </div>
+                {children}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
         </div>
       </Dialog>
     </Transition.Root>
