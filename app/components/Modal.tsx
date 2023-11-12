e2894f84e8efc98dc1df471553c17320f5e2ea9f
Simon Yoo <yoosghn0@gmail.com>
2023-11-12 16:52:27 +1300

    interface props

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,6 +1,11 @@
 'use client'
 
-const Modal = () => {
+interface ModalProps {
+  isOpen: boolean
+  onClose: () => void
+  children: React.ReactNode
+}
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   return <div>Modal</div>
 }
 
