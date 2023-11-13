82015f0cc68fca0fe335cee1eec4536d8e7d3500
Simon Yoo <yoosghn0@gmail.com>
2023-11-14 12:17:31 +1300

    import ActiveStatus

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,31 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import ToasterContext from './context/ToasterContext';
-import AuthContext from './context/AuthContext';
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import ToasterContext from './context/ToasterContext'
+import AuthContext from './context/AuthContext'
+import ActiveStatus from './components/ActiveStatus'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Messenger Clone',
   description: 'Messenger Clone',
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
         <AuthContext>
           <ToasterContext />
+          <ActiveStatus />
           {children}
         </AuthContext>
       </body>
     </html>
-  );
+  )
 }
