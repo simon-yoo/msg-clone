6916791311c9fc12dd1108de89eb7e047daa1091
Simon Yoo <yoosghn0@gmail.com>
2023-11-13 19:48:31 +1300

    LoadingModal component

--- app/components/LoadingModal.tsx
+++ app/components/LoadingModal.tsx
@@ -5,7 +5,23 @@ import { Dialog, Transition } from '@headlessui/react'
 import { ClipLoader } from 'react-spinners'
 
 const LoadingModal = () => {
-  return <div>LoadingModal</div>
+  return (
+    <Transition.Root show as={Fragment}>
+      <Dialog as='div' className='relative z-50' onClose={() => {}}>
+        <Transition.Child
+          as={Fragment}
+          enter='ease-out duration-300'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in duration-200'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='fixed inset-0 bg-gray-100 bg-opacity-50 transition-opacity' />
+        </Transition.Child>
+      </Dialog>
+    </Transition.Root>
+  )
 }
 
 export default LoadingModal
