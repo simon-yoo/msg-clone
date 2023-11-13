073271ee222999ddacec1425bffc2cbc76212b8c
Simon Yoo <yoosghn0@gmail.com>
2023-11-13 16:37:11 +1300

    create props

--- app/components/input/Select.tsx
+++ app/components/input/Select.tsx
@@ -1,6 +1,20 @@
 'use client'
 
-const Select = () => {
+interface SelectProps {
+  label: string
+  value?: Record<string, any>
+  onChange: (value: Record<string, any>) => void
+  options: Record<string, any>[]
+  disabled?: boolean
+}
+
+const Select: React.FC<SelectProps> = ({
+  label,
+  value,
+  onChange,
+  options,
+  disabled,
+}) => {
   return <div>Select</div>
 }
 
