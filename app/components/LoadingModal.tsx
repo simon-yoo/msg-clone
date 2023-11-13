f9c2054843dcc91b57ef90a746315ff9ecb91e08
Simon Yoo <yoosghn0@gmail.com>
2023-11-13 19:43:31 +1300

    create LoadingModal component

---
+++ app/components/LoadingModal.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React, { Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { ClipLoader } from 'react-spinners'
+
+const LoadingModal = () => {
+  return <div>LoadingModal</div>
+}
+
+export default LoadingModal
