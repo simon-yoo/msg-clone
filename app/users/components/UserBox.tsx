a6de24c077e327a4be3a4cfa8d6cab7a4132a31f
Simon Yoo <yoosghn0@gmail.com>
2023-11-13 19:53:28 +1300

    dynamic isloading

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -30,7 +30,7 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
 
   return (
     <>
-      <LoadingModal />
+      {isLoading && <LoadingModal />}
       <div
         onClick={handleClick}
         className='w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer'
