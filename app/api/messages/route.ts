b6fe9251479d3ba4599c30d3d1c745e5b47a3a21
Simon Yoo <yoosghn0@gmail.com>
2023-11-13 20:15:33 +1300

    route pusherServer

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -61,6 +61,17 @@ export async function POST(request: Request) {
       },
     })
 
+    await pusherServer.trigger(conversationId, 'messages:new', newMessage)
+
+    const lastMessage =
+      updatedConversation.messages[updatedConversation.messages.length - 1]
+
+    updatedConversation.users.map((user) => {
+      pusherServer.trigger(user.email!, 'conversation:update', {
+        id: conversationId,
+        messages: [lastMessage],
+      })
+    })
     return NextResponse.json(newMessage)
   } catch (error: any) {
     console.log(error, 'ERROR_MESSAGES')
