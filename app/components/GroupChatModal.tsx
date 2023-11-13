30772a245e3152208e178d46eb7d0c4aa810872e
Simon Yoo <yoosghn0@gmail.com>
2023-11-13 16:08:36 +1300

    add users in interface

--- app/components/GroupChatModal.tsx
+++ app/components/GroupChatModal.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import { User } from '@prisma/client'
+
 interface GroupChatModalProps {
   isOpen?: boolean
   onClose: () => void
+  users: User[]
 }
 const GroupChatModal: React.FC<GroupChatModalProps> = ({ isOpen, onClose }) => {
   return <div>GroupChatModal</div>
