28792bcc24e0350087a841157977e7cf1e73938e
Simon Yoo <yoosghn0@gmail.com>
2023-11-13 15:15:10 +1300

    settingmodal profile

--- app/components/sidebar/SettingsModal.tsx
+++ app/components/sidebar/SettingsModal.tsx
@@ -7,6 +7,10 @@ import { useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
 import Modal from '../Modal'
+import Input from '../input/Input'
+import Image from 'next/image'
+import { CldUploadButton } from 'next-cloudinary'
+import Button from '../Button'
 
 interface SettingsModalProps {
   isOpen?: boolean
@@ -57,7 +61,51 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div></div>
+        <div className='space-y-12'>
+          <div className='border-b border-gray-900/10 pb-12'>
+            <h2 className='text-base font-semibold leading-7 text-gray-900'>
+              Profile
+            </h2>
+            <p className='mt-1 text-sm leading-6 text-gray-600'>
+              Edit your public information.
+            </p>
+            <div className='mt-10 flex flex-col gap-y-8'>
+              <Input
+                disabled={isLoading}
+                label='Name'
+                id='name'
+                errors={errors}
+                required
+                register={register}
+              />
+              <div>
+                <label className='block text-sm font-medium leading-6 text-gray-900'>
+                  Photo
+                </label>
+                <div className='mt-2 flex items-center gap-x-3'>
+                  <Image
+                    width={48}
+                    height={48}
+                    className='rounded-full'
+                    src={
+                      image || currentUser?.image || '/images/placeholder.jpg'
+                    }
+                    alt='Avatar'
+                  />
+                  <CldUploadButton
+                    options={{ maxFiles: 1 }}
+                    onUpload={handleUpload}
+                    uploadPreset='messenger'
+                  >
+                    <Button disabled={isLoading} secondary type='button'>
+                      Change
+                    </Button>
+                  </CldUploadButton>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </form>
     </Modal>
   )
