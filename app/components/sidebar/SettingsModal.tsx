f0674c54ac2785a82b1873f7f715bba7a293f107
Simon Yoo <yoosghn0@gmail.com>
2023-11-13 15:04:38 +1300

    SettingsModal ing

--- app/components/sidebar/SettingsModal.tsx
+++ app/components/sidebar/SettingsModal.tsx
@@ -1,7 +1,66 @@
 'use client'
 
-const SettingsModal = () => {
-  return <div>SettingsModal</div>
+import { User } from '@prisma/client'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
+import Modal from '../Modal'
+
+interface SettingsModalProps {
+  isOpen?: boolean
+  onClose: () => void
+  currentUser: User
+}
+const SettingsModal: React.FC<SettingsModalProps> = ({
+  isOpen,
+  onClose,
+  currentUser,
+}) => {
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: currentUser?.name,
+      image: currentUser?.image,
+    },
+  })
+
+  const image = watch('image')
+
+  const handleUpload = (result: any) => {
+    setValue('image', result?.info?.secure_url, {
+      shouldValidate: true,
+    })
+  }
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true)
+
+    axios
+      .post('/api/settings', data)
+      .then(() => {
+        router.refresh()
+        onClose()
+      })
+      .catch(() => toast.error('Something went wrong'))
+      .finally(() => setIsLoading(false))
+  }
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div></div>
+      </form>
+    </Modal>
+  )
 }
 
 export default SettingsModal
